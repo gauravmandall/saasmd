aa4900170679de57931614e4e1b8ffc79c32e94e
gauravmandall <mandalgaurav550@gmail.com>
2024-06-05 10:49:02 +0530

    v0.1.0 to production

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import { initProject } from "./commands/init/index.js";
+import { buildSchema } from "./commands/generate/index.js";
+import { addPackage } from "./commands/add/index.js";
+import { toggleAnalytics } from "./commands/init/utils.js";
+
+const program = new Command();
+program.name("saasmd").description("Saasmd CLI").version("0.1.0");
+
+program
+  .command("analytics")
+  .option("-t, --toggle", "toggle anonymous analytics")
+  .action(toggleAnalytics);
+
+addCommonOptions(program.command("init"))
+  .description("initialise and configure saasmd within directory")
+  .action(initProject);
+
+program
+  .command("generate")
+  .description("Generate a new resource")
+  .action(buildSchema);
+
+addCommonOptions(program.command("add"))
+  .description("Add and setup additional packages")
+  .action(addPackage);
+
+program.parse(process.argv);
+
+function addCommonOptions(command: Command) {
+  return command
+    .option("-sf, --has-src-folder", "has a src folder")
+    .option(
+      "-pm, --package-manager <pm>",
+      "preferred package manager (npm, yarn, pnpm, bun)"
+    )
+    .option(
+      "-cl, --component-lib <component-lib>",
+      "preferred component library (shadcn-ui)"
+    )
+    .option("-o, --orm <orm>", "preferred orm (prisma, drizzle)")
+    .option("-db, --db <db>", "preferred database (pg, mysql, sqlite)")
+    .option("-dbp, --db-provider <db>", "database provider")
+    .option("-a, --auth <auth>", "preferred auth (next-auth, clerk, lucia)")
+    .option(
+      "-ap, --auth-providers <auth-providers...>",
+      "auth providers (if using next-auth - discord, google, github, apple)"
+    )
+    .option(
+      "-mp, --misc-packages <packages...>",
+      "misc packages (resend, stripe, trpc)"
+    )
+    .option("-ie, --include-example", "include example model in schema");
+}
+
+process.on("SIGINT", () => {
+  // Then end process
+  process.exit();
+});
